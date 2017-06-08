9e930a9813ee064f7d04e4e695cb459a036568c2
Benjamin Schweizer <cxcv@cxcv.de>
2017-06-08 15:48:48 +0200

    complete scheme

---
+++ rainbo.sh
@@ -0,0 +1,21 @@
+printf "[48;5;246m          "
+printf "[48;5;160m          "
+printf "[48;5;208m          "
+printf "[48;5;011m          "
+printf "[48;5;041m          "
+printf "[48;5;044m          "
+printf "[0m\n"
+printf "\n"
+colortrans.py 949494 # grey
+colortrans.py d70000 # red
+colortrans.py ff8700 # orange
+colortrans.py ffff00 # yellow
+colortrans.py 00d75f # green
+colortrans.py 00d7d7 # cyan
+echo
+colortrans.py 121212 # black
+colortrans.py 5f0000 # red
+colortrans.py af5f00 # orange
+colortrans.py afaf00 # yellow
+colortrans.py 005f00 # green
+colortrans.py 005f5f # cyan
